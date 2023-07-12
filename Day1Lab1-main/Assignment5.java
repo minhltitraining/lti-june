502d108d6aef9c7d7ab3b119c79a198c9c42550d
minhltitraining <137228447+minhltitraining@users.noreply.github.com>
2023-07-12 14:11:50 +0000

    Day1Lab1

---
+++ Day1Lab1-main/Assignment5.java
@@ -0,0 +1,12 @@
+//Program to understand local variables and literals
+class Variable {
+    public static void main(String args[]) {
+        int intVal = 10;
+        float floatVal = 250.5f;
+        double doubleVal = 2500.5;
+        boolean boolVal = true;
+        System.out.println(
+                "Integer\t:" + intVal + "\nFloat\t:" + floatVal + "\nDouble\t:" + 
+                doubleVal + "\nBoolean\t:" + boolVal);
+    }
+}
---
+++ Day1Lab1-main/Assignment8.java
@@ -0,0 +1,12 @@
+// Program to understand different types of operators
+class Operator {
+    public static void main (String[] args) {
+        int intVal = 100;
+        byte byteVal = (byte) intVal;
+        byte max = 127;
+        byte min = -128;
+        byte sum = max + min;
+
+        System.out.println("Sum= " + sum);
+    }
+}
